33341ef4bc9a97ac1b55b18bb88de1a93064231e
jnnkB <29376715+jnnkB@users.noreply.github.com>
2021-01-15 20:43:33 +0100

    cleaned up fix.py

--- fix.py
+++ fix.py
@@ -1,20 +1,36 @@
 import sys
 import itertools
+import csv
 
-with open(sys.argv[2], "r") as f:
-    text = f.read()
 
-for front_char, back_char in itertools.product([" ", ".", ""], repeat=2):
-    with open(sys.argv[1], "r") as f2:
-        for line in f2.readlines():
-            if ";" in line:
-                i, j = line[:-1].split(";")
-                if front_char + i + back_char in text:
-                    text = text.replace(
-                        front_char + i + back_char, front_char + j + back_char
-                    )
-                    print("replaced", i, j)
-        f2.seek(0)
+def read_word_list(csvfile):
+    reader = csv.reader(csvfile, delimiter=";", quotechar='"')
+    return list(reader)
 
-with open(sys.argv[2], "w") as f:
-    f.write(text)
+
+def replace_words(text, word_list):
+    # These front and back chars try to ensure that if possible whole words are replaced.
+    for front_char, back_char in itertools.product([" ", ".", ""], repeat=2):
+        for fixed_word, encoding_error_word in word_list:
+            fixed_string = front_char + encoding_error_word + back_char
+            if fixed_string in text:
+                encoding_error_string = front_char + fixed_word + back_char
+                text = text.replace(
+                    fixed_string,
+                    encoding_error_string,
+                )
+                print(f"replaced '{encoding_error_string}' with '{fixed_string}'")
+    return text
+
+
+if __name__ == "__main__":
+    with open(sys.argv[2], "r") as f:
+        text = f.read()
+
+    with open(sys.argv[1], "r") as csvfile:
+        word_list = read_word_list(csvfile)
+
+    replace_words(text, word_list)
+
+    with open(sys.argv[2], "w") as f:
+        f.write(text)
